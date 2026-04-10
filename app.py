c38918de75e120056ecd59d4884427c9540d4bf1
sanjaypoojary56-art <sanjaypoojary56@gmail.com>
2026-04-10 20:12:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -117,5 +117,5 @@ def home():
 
 # 🔥 Run
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)    
